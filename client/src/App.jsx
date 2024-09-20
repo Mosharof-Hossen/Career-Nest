886c6716e0449fc27bb4945151c25eb7dfa2519b
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-09-20 21:44:39 +0600

    framer motion scroll used

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,23 +3,47 @@ import './App.css'
 import Footer from './Components/shared/Footer'
 import Navbar from './Components/shared/Navbar'
 import { Outlet } from "react-router-dom";
+import { motion, useScroll, useSpring } from "framer-motion";
+
 
 function App() {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
+
 
 
   return (
-    <div className='bg-white text-black dark:bg-gray-900 dark:text-white '>
-      <div className='flex flex-col min-h-screen max-w-6xl mx-auto'>
-        <div className='flex-grow'>
-          <Navbar></Navbar>
-          <Outlet />
-        </div>
+    <>
+      <motion.div
+        style={{
+          scaleX: scaleX, // Animate scaling of an element as the page scrolls
+          height: "10px",
+          background: "red",
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          transformOrigin: 0,
+          zIndex:10
+        }}
+      />
+      <div className='bg-white text-black dark:bg-gray-900 dark:text-white '>
+        <div className='flex flex-col min-h-screen max-w-6xl mx-auto'>
+          <div className='flex-grow'>
+            <Navbar></Navbar>
+            <Outlet />
+          </div>
 
-        <div>
-          <Footer></Footer>
+          <div>
+            <Footer></Footer>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- client/src/Pages/Blogs/Blogs.jsx
+++ client/src/Pages/Blogs/Blogs.jsx
@@ -1,8 +1,10 @@
 
+
 const Blogs = () => {
+    
     return (
         <div>
-            Blogs
+
         </div>
     );
 };
