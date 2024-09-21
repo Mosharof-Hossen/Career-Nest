c5f1d7bf8e4ef52ee967856a657e1238a0ddef33
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-09-21 15:15:08 +0600

    responsive bug fixed

--- client/src/Components/ExploreCategory/ExploreCategory.jsx
+++ client/src/Components/ExploreCategory/ExploreCategory.jsx
@@ -11,42 +11,42 @@ const ExploreCategory = () => {
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
                 <div className="text-center bg-slate-100 rounded space-y-3 dark:bg-gray-800 p-10">
                     <FaCalculator className="text-primary-c mx-auto text-5xl" />
-                    <h5 className="text-2xl font-bold">Accounting & Finance</h5>
+                    <h5 className="text-xl font-bold">Accounting & Finance</h5>
                     <p>122 Active Jobs</p>
                 </div>
                 <div className="text-center bg-slate-100 rounded space-y-3 dark:bg-gray-800 p-10">
-                    <FaAccessibleIcon  className="text-primary-c mx-auto text-5xl" />
-                    <h5 className="text-2xl font-bold">Automotive Jobs</h5>
+                    <FaAccessibleIcon className="text-primary-c mx-auto text-5xl" />
+                    <h5 className="text-xl font-bold">Automotive Jobs</h5>
                     <p>78 Active Jobs</p>
                 </div>
                 <div className="text-center bg-slate-100 rounded space-y-3 dark:bg-gray-800 p-10">
-                    <FaAndroid  className="text-primary-c mx-auto text-5xl" />
-                    <h5 className="text-2xl font-bold">Business & Tech</h5>
+                    <FaAndroid className="text-primary-c mx-auto text-5xl" />
+                    <h5 className="text-xl font-bold">Business & Tech</h5>
                     <p>25 Active Jobs</p>
                 </div>
                 <div className="text-center bg-slate-100 rounded space-y-3 dark:bg-gray-800 p-10">
-                    <FaBookReader  className="text-primary-c mx-auto text-5xl" />
-                    <h5 className="text-2xl font-bold">Education Training</h5>
+                    <FaBookReader className="text-primary-c mx-auto text-5xl" />
+                    <h5 className="text-xl font-bold">Education Training</h5>
                     <p>212 Active Jobs</p>
                 </div>
                 <div className="text-center bg-slate-100 rounded space-y-3 dark:bg-gray-800 p-10">
-                    <FaBriefcaseMedical  className="text-primary-c mx-auto text-5xl" />
-                    <h5 className="text-2xl font-bold">Healthcare</h5>
+                    <FaBriefcaseMedical className="text-primary-c mx-auto text-5xl" />
+                    <h5 className="text-xl font-bold">Healthcare</h5>
                     <p>90 Active Jobs</p>
                 </div>
                 <div className="text-center bg-slate-100 rounded space-y-3 dark:bg-gray-800 p-10">
-                    <IoRestaurantSharp  className="text-primary-c mx-auto text-5xl" />
-                    <h5 className="text-2xl font-bold">Restaurant & Food</h5>
+                    <IoRestaurantSharp className="text-primary-c mx-auto text-5xl" />
+                    <h5 className="text-xl font-bold">Restaurant & Food</h5>
                     <p>65 Active Jobs</p>
                 </div>
                 <div className="text-center bg-slate-100 rounded space-y-3 dark:bg-gray-800 p-10">
-                    <FaTrain  className="text-primary-c mx-auto text-5xl" />
-                    <h5 className="text-2xl font-bold">Transportation</h5>
+                    <FaTrain className="text-primary-c mx-auto text-5xl" />
+                    <h5 className="text-xl font-bold">Transportation</h5>
                     <p>160 Active Jobs</p>
                 </div>
                 <div className="text-center bg-slate-100 rounded space-y-3 dark:bg-gray-800 p-10">
                     <FaPhone className="text-primary-c mx-auto text-5xl" />
-                    <h5 className="text-2xl font-bold">Tele- communications</h5>
+                    <h5 className="text-xl font-bold">Community</h5>
                     <p>80 Active Jobs</p>
                 </div>
             </div>
--- client/src/Components/Stat/Stat.jsx
+++ client/src/Components/Stat/Stat.jsx
@@ -1,25 +1,25 @@
 
 const Stat = () => {
     return (
-        <div className="mt-14 text-center">
-            <div className="stats shadow dark:bg-gray-800 ">
-                <div className="stat place-items-center space-y-2 p-12 text-center">
-                    <div className="stat-value text-primary-c">20K</div>
-                    <div className="stat-desc dark:text-white">Happy Employer</div>
+        <div className="mt-14 text-center p-5">
+            <div className=" grid grid-cols-2 gap-2  dark:bg-gray-800 ">
+                <div className="border rounded place-items-center space-y-2 p-12 text-center">
+                    <div className="text-5xl font-bold text-primary-c">20K</div>
+                    <div className=" dark:text-white">Happy Employer</div>
                 </div>
 
-                <div className="stat place-items-center space-y-2  p-12 text-center">
-                    <div className="stat-value text-primary-c">11K</div>
-                    <div className="stat-desc dark:text-white">Opening Position</div>
+                <div className="border rounded place-items-center space-y-2  p-12 text-center">
+                    <div className="text-5xl font-bold text-primary-c">11K</div>
+                    <div className=" dark:text-white">Opening Position</div>
                 </div>
 
-                <div className="stat place-items-center space-y-2  p-12 text-center">
-                    <div className="stat-value text-primary-c" >1K</div>
-                    <div className="stat-desc dark:text-white"> Daily Active User</div>
+                <div className="border rounded place-items-center space-y-2  p-12 text-center">
+                    <div className="text-5xl font-bold text-primary-c" >1K</div>
+                    <div className=" dark:text-white"> Daily Active User</div>
                 </div>
-                <div className="stat place-items-center space-y-2  p-12 text-center">
-                    <div className="stat-value text-primary-c" >5K</div>
-                    <div className="stat-desc dark:text-white"> Job Categories</div>
+                <div className="border rounded place-items-center space-y-2  p-12 text-center">
+                    <div className="text-5xl font-bold text-primary-c" >5K</div>
+                    <div className=" dark:text-white"> Job Categories</div>
                 </div>
             </div>
         </div>
